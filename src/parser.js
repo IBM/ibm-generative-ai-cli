73a2ea44e0fcadc57fb459442050875b1b6fc68a
Tomas Pilar <Tomas.Pilar@ibm.com>
2023-07-27 09:25:58 +0200

    feat(tunes): tunes command
    Signed-off-by: Tomas Pilar <Tomas.Pilar@ibm.com>

--- src/parser.js
+++ src/parser.js
@@ -14,6 +14,7 @@ import {
   FilePurposeSchema,
   HistoryStatusSchema,
   HistoryOriginSchema,
+  TuneAssetTypeSchema,
 } from "@ibm-generative-ai/node-sdk";
 
 import { parseDateTime, parseInput } from "./utils/parsers.js";
@@ -895,6 +896,225 @@ export const parser = yargs(hideBin(process.argv))
         }
       }
     )
+  )
+  .command("tunes", "Create and manage tuned models", (yargs) =>
+    yargs
+      .middleware(clientMiddleware)
+      .command("methods", "List all tune methods", {}, async (args) => {
+        const methods = await args.client.tuneMethods();
+        methods.forEach((method) => {
+          console.log(`${method.id} (${method.name})`);
+        });
+      })
+      .command(
+        "list",
+        "List all tuned models",
+        groupOptions({
+          name: {
+            type: "string",
+            description: "Filter tuned models by name",
+            requiresArg: true,
+          },
+        }),
+        async (args) => {
+          for await (const tune of args.client.tunes({ search: args.name })) {
+            console.log(`${tune.id} (${tune.name})`);
+          }
+        }
+      )
+      .command(
+        "info <id>",
+        "Show detailed information about a tuned model",
+        (yargs) =>
+          yargs.positional("id", {
+            type: "string",
+            description: "Identifier of the tuned model",
+          }),
+        async (args) => {
+          const { id, name } = await args.client.tune({
+            id: args.id,
+          });
+          prettyPrint({ id, name });
+        }
+      )
+      .command(
+        "create",
+        "Create a tuned model",
+        (yargs) =>
+          yargs
+            .options(
+              groupOptions({
+                name: {
+                  type: "string",
+                  description: "Name of the tuned model",
+                  requiresArg: true,
+                  demandOption: true,
+                },
+                model: {
+                  type: "string",
+                  description: "Model to be tuned",
+                  requiresArg: true,
+                  demandOption: true,
+                },
+                task: {
+                  description: "Tuning task",
+                  requiresArg: true,
+                  demandOption: true,
+                  choices: ["generation", "classification", "summarization"],
+                },
+                method: {
+                  type: "string",
+                  description: "Tuning method",
+                  requiresArg: true,
+                  demandOption: true,
+                },
+                training: {
+                  type: "array",
+                  description: "Uploaded files to be used for training",
+                  requiresArg: true,
+                  demandOption: true,
+                },
+                validation: {
+                  type: "array",
+                  description: "Uploaded files to be used for validation",
+                  requiresArg: true,
+                },
+                evaluation: {
+                  type: "array",
+                  description: "Uploaded files to be used for evaluation",
+                  requiresArg: true,
+                },
+              })
+            )
+            .options(
+              groupOptions(
+                {
+                  "accumulate-steps": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 16,
+                    description:
+                      "Number of training steps to use to combine gradients",
+                  },
+                  "batch-size": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 16,
+                    description:
+                      "Number of samples to work through before updating the internal model parameters",
+                  },
+                  "learning-rate": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 0.3,
+                    description:
+                      "Learning rate to be used while tuning prompt vectors",
+                  },
+                  "max-input-tokens": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 256,
+                    description:
+                      "Maximum number of tokens that are accepted in the input field for each example",
+                  },
+                  "max-output-tokens": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 128,
+                    description:
+                      "Maximum number of tokens that are accepted in the output field for each example",
+                  },
+                  "num-epochs": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 20,
+                    description:
+                      "The number of times to cycle through the training data set",
+                  },
+                  "num-virtual-tokens": {
+                    type: "number",
+                    requiresArg: true,
+                    default: 100,
+                    description:
+                      "Number of virtual tokens to be used for training",
+                  },
+                  verbalizer: {
+                    type: "string",
+                    requiresArg: true,
+                    description:
+                      "Verbalizer template to be used for formatting data at train and inference time",
+                  },
+                },
+                "Parameters:"
+              )
+            )
+            .middleware((args) => {
+              const parameters = pickDefined({
+                accumulate_steps: args.accumulateSteps,
+                batch_size: args.batchSize,
+                init_method: args.initMethod,
+                init_text: args.initText,
+                learning_rate: args.learningRate,
+                max_input_tokens: args.maxInputTokens,
+                max_output_tokens: args.maxOutputTokens,
+                num_epochs: args.numEpochs,
+                num_virtual_tokens: args.numVirtualTokens,
+                verbalizer: args.verbalizer,
+              });
+              args.parameters = !_.isEmpty(parameters) ? parameters : undefined;
+            }),
+        async (args) => {
+          const { id, name } = await args.client.tune({
+            name: args.name,
+            model_id: args.model,
+            task_id: args.task,
+            method_id: args.method,
+            parameters: args.parameters,
+            training_file_ids: args.training,
+            validation_file_ids: args.validation,
+            evaluation_file_ids: args.evaluation,
+          });
+          prettyPrint({ id, name });
+        }
+      )
+      .command(
+        "download <asset> <id>",
+        "Download assets of a completed tuned model",
+        (yargs) =>
+          yargs
+            .positional("id", {
+              type: "string",
+              description: "Identifier of the tuned model",
+            })
+            .positional("asset", {
+              describe: "Type of the asset",
+              choices: TuneAssetTypeSchema.options,
+            }),
+        async (args) => {
+          const { downloadAsset, status } = await args.client.tune({
+            id: args.id,
+          });
+          if (status !== "COMPLETED")
+            throw new BaseSDKError(
+              "Only completed tunes have assets available"
+            );
+          const readable = await downloadAsset(args.type);
+          await pipeline(readable, args.output ?? stdout);
+        }
+      )
+      .command(
+        "delete <id>",
+        "Delete a tuned model",
+        (yargs) =>
+          yargs.positional("id", {
+            type: "string",
+            description: "Identifier of the tuned model",
+          }),
+        async (args) => {
+          await args.client.tune({ id: args.id }, { delete: true });
+        }
+      )
+      .demandCommand(1, 1, "Please choose a command")
   )
   .demandCommand(1, 1, "Please choose a command")
   .config(loadConfig().configuration)
