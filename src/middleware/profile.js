0bd5ac570da410aa73030f604574c634fd7fb00c
Tomas Pilar <tomas.pilar@ibm.com>
2023-08-31 15:15:12 +0200

    fix(config): new profile creation
    Signed-off-by: Tomas Pilar <tomas.pilar@ibm.com>

--- src/middleware/profile.js
+++ src/middleware/profile.js
@@ -3,8 +3,10 @@ import { allProfiles, loadProfileConfig } from "../utils/config.js";
 // If profile has been selected, lazy load its configuration into yargs
 export function profileMiddleware(args) {
   if (!args.profile) return;
-  if (!allProfiles().includes(args.profile))
+  if (!allProfiles().includes(args.profile)) {
+    if (args._.length === 1 && args._[0] === "config") return; // Only create config command can pass
     throw new Error("Profile not found");
+  }
   Object.entries(loadProfileConfig(args.profile)).forEach(([key, value]) => {
     args[key] = value;
   });
